93775cfe1f2b8b9226c0dc7cf698cbb470dabaf1
Marcosespa <marcosespa24@gmail.com>
2024-12-03 10:32:05 -0500

    index principal pendeinte deplyment

--- ofipensiones/urls.py
+++ ofipensiones/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from ofipensiones.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', inicio, name='inicio'),
     path('facturacion/', include('facturación.urls')),
 ]
