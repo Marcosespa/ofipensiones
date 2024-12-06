d525bf237273524fd214bff6c81364b79fefa998
Valentina Ceron <l.ceronp@uniandes.edu.co>
2024-12-05 20:21:50 -0500

    funciones definidas en reportes

--- reportes/views.py
+++ reportes/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from .models import Factura
 
 def vista_reporte_facturas(request):
-   
-    reporte = Factura.generar_reporte_facturas()
+    facturas = Factura.objects.all()
+    reporte = [
+        {
+            'id': factura.id,
+            'estudiante': factura.estudiante.nombre,
+            'estado': factura.estado_actual(), 
+            'saldo_pendiente': factura.calcular_saldo_pendiente(),  
+            'monto_total': factura.monto_total,
+            'fecha_emision': factura.fecha_emision,
+        }
+        for factura in facturas
+    ]
     return render(request, 'reporte_facturas.html', {'reporte': reporte})
